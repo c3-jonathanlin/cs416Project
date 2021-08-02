8d207768887cac1cba2abcb4687202dcf4768e78
Jonathan Lin <jonathan.lin@c3.ai>
2021-08-01 20:28:37 -0700

    tooltip on screen 3 and text edits

--- js/chart3.js
+++ js/chart3.js
@@ -10,6 +10,34 @@ var svg3 = d3.select("#scene3")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+var tooltip3 = d3.select("#scene2")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "black")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+
+var mouseover3 = function (d) {
+    tooltip3.style("opacity", 1)
+}
+
+var mousemove3 = function (d) {
+    tooltip3
+        .html("County: " + d.county)
+        .style("left", (d3.mouse(this)[0]+150) + "px")
+        .style("top", (d3.mouse(this)[1]+100) + "px")
+}
+
+var mouseleave3 = function (d) {
+    tooltip3
+        .transition()
+        .duration(200)
+        .style("opacity", 0)
+}
+
 d3.csv("https://raw.githubusercontent.com/c3-jonathanlin/cs416Project/master/data/vaccination_ca_july.csv", function (data) {
   var x3 = d3.scaleLinear()
     .domain([-1, 1])
@@ -17,13 +45,11 @@ d3.csv("https://raw.githubusercontent.com/c3-jonathanlin/cs416Project/master/dat
   svg3.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x3));
-
   var y3 = d3.scaleLinear()
     .domain([0, 1])
     .range([ height, 0]);
   svg3.append("g")
     .call(d3.axisLeft(y3));
-
   svg3.append('g')
     .selectAll("dot")
     .data(data)
@@ -34,4 +60,7 @@ d3.csv("https://raw.githubusercontent.com/c3-jonathanlin/cs416Project/master/dat
       .attr("r", 5)
       .style("opacity", 0.5)
       .style("fill", "#000000")
+    .on("mouseover", mouseover3)
+    .on("mousemove", mousemove3)
+    .on("mouseleave", mouseleave3)
 });
